40430146e049bd68e71517daaecb597fa3dbdb73
Ralfazza Rajariandhana <rajariandhana@gmail.com>
2025-06-23 15:14:46 +1000

    UI Auth Login

---
+++ src/components/views/Auth/Login/Login.tsx
@@ -0,0 +1,74 @@
+import Image from "next/image";
+import { Card,CardBody,Input,Button, Spinner } from "@nextui-org/react";
+import Link from "next/link";
+import useLogin from "./useLogin";
+import { FaEye,FaEyeSlash } from "react-icons/fa6";
+import { Controller } from "react-hook-form";
+import { cn } from "@/utils/cn";
+
+const Login = () => {
+    const {isVisible, toggleVisibility, control, handleSubmit, handleLogin, isPendingLogin, errors} = useLogin();
+    // console.log(errors);
+    return (
+        <div className="flex w-full flex-col lg:flex-row items-center justify-center gap-10 lg:gap-20">
+            <div className="flex w-full lg:w-1/3 flex-col items-center justify-center gap-10">
+                <Image
+                    src="/images/general/logo.svg"
+                    alt="logo"
+                    width={180}
+                    height={180}>
+                </Image>
+                <Image
+                    src="/images/illustrations/login.svg"
+                    alt="login"
+                    className="w-2/3 lg:w-full"
+                    width={1024}
+                    height={1024}>
+                </Image>
+            </div>
+            <Card>
+                <CardBody className="p-8">
+                    <h2 className="text-xl font-bold text-danger-500">Login</h2>
+                    <p className="text-sm mb-4">Don't have an account?&nbsp;
+                        <Link href="/auth/register" className="font-semibold text-danger-400">
+                        Register here
+                        </Link>
+                    </p>
+                    {errors.root && 
+                        (<p className="mb-2 font-medium text-danger">
+                            {errors?.root?.message}
+                        </p>)
+                    }
+                    <form className={cn("flex w-80 flex-col", Object.keys(errors).length > 0 ? "gap-2":"gap-4")} onSubmit={handleSubmit(handleLogin)}>
+                        <Controller name="identifier" control={control} render={({field}) => 
+                            <Input {...field} type="text" label="Username or Email" variant="bordered" autoComplete="off">
+                                isInvalid={errors.identifier !== undefined} errorMessage={errors.identifier?.message}
+                            </Input>
+                        }/>
+                        <Controller name="password" control={control} render={({field}) => 
+                            <Input {...field} type={isVisible ? 'text':'password'} label="Password" variant="bordered" autoComplete="off"
+                                isInvalid={errors.password !== undefined} errorMessage={errors.password?.message}
+                                endContent={
+                                    <button className="foxcus:outline-none"
+                                    type="button"
+                                    onClick={() => toggleVisibility()}>
+                                        {isVisible ? (
+                                            <FaEye className="text-xl text-default-400 pointer-events-none"/>
+                                        ):(
+                                            <FaEyeSlash className="text-xl text-default-400 pointer-events-none"/>
+                                )}
+                                </button>
+                                }>
+                    
+                            </Input>
+                        }/>
+                        <Button color="danger" size="lg" type="submit">
+                            {isPendingLogin ? <Spinner color="white" size="sm"/> : 'Login'}
+                        </Button>
+                    </form>
+                </CardBody>
+            </Card>
+        </div>
+    )
+}
+export default Login;
---
+++ src/components/views/Auth/Login/useLogin.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react"
+import * as yup from "yup";
+import {useForm} from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { ILogin } from "@/types/Auth";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { signIn } from "next-auth/react";
+
+const loginSchema = yup.object().shape({
+    identifier: yup.string().required("Please input your username or email"),
+    password: yup.string().required("Please input your password"),
+});
+
+const useLogin = () => {
+    const router = useRouter();
+    const [isVisible, setIsVisible] = useState(false);
+
+    const callBackURL: string = (router.query.callBackURL as string) || "/";
+
+    const toggleVisibility = () => {
+        setIsVisible(!isVisible);
+    }
+
+    const {control, handleSubmit,formState:{errors},reset,setError} = useForm({
+        resolver: yupResolver(loginSchema),
+    });
+
+    const loginService = async (payload:ILogin) => {
+        const result = await signIn("credentials",{
+            ...payload,
+            redirect: false,
+            callbackUrl: callBackURL,
+        });
+        if(result?.error && result?.status===401){
+            throw new Error("Invalid credentials");
+        }
+        return result;
+    };
+
+    const {mutate: mutateLogin, isPending: isPendingLogin} = useMutation({
+        mutationFn: loginService,
+        onError(error) {
+            setError("root",{
+                message: error.message,
+            });
+        },
+        onSuccess: () =>{
+            router.push(callBackURL);
+            reset();
+        }
+    });
+
+    const handleLogin = (data: ILogin) => mutateLogin(data);
+
+    return {
+        isVisible, toggleVisibility, control, handleSubmit, handleLogin, isPendingLogin, errors
+    }
+}
+
+export default useLogin;
