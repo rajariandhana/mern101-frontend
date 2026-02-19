32fdc4f8bd3f9504261b8f570d8ebbc196b0c0c1
Ralfazza Rajariandhana <s4949323@student.uq.edu.au>
2026-02-19 11:00:44 +1000

    Search, Limit & Pagination

--- src/components/views/Admin/Category/Category.tsx
+++ src/components/views/Admin/Category/Category.tsx
@@ -1,39 +1,48 @@
 import DataTable from "@/components/ui/DataTable/DataTable";
-import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/react";
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { Key, ReactNode, useCallback } from "react";
 import { CiMenuKebab } from "react-icons/ci";
 import { COLUMN_LISTS_CATEGORY } from "./Category.constants";
+import { LIMIT_LISTS } from "@/constants/list.constant";
 
 const Category = () => {
-  const {push} = useRouter();
+  const { push } = useRouter();
   const renderCell = useCallback(
     (category: Record<string, unknown>, columnKey: Key) => {
-      const cellValue = category[columnKey as keyof typeof category]
-      switch(columnKey){
+      const cellValue = category[columnKey as keyof typeof category];
+      switch (columnKey) {
         case "icon":
           return (
-            <Image src={`${cellValue}`} alt="icon" width={100} height={200}/>
+            <Image src={`${cellValue}`} alt="icon" width={100} height={200} />
           );
         case "actions":
           return (
             <Dropdown>
               <DropdownTrigger>
                 <Button isIconOnly size="sm" variant="light">
-                  <CiMenuKebab className="text-default-700"/>
+                  <CiMenuKebab className="text-default-700" />
                 </Button>
               </DropdownTrigger>
               <DropdownMenu>
                 <DropdownItem
                   onPress={() => push(`/admin/category/${category._id}`)}
-                  key={"detail-category-button"}>
-                    Detail Category
-                  </DropdownItem>
+                  key={"detail-category-button"}
+                >
+                  Detail Category
+                </DropdownItem>
                 <DropdownItem
                   key={"delete-category-button"}
-                  className="text-danger-500">
-                    Delete Category
+                  className="text-danger-500"
+                >
+                  Delete Category
                 </DropdownItem>
               </DropdownMenu>
             </Dropdown>
@@ -41,8 +50,9 @@ const Category = () => {
         default:
           return cellValue as ReactNode;
       }
-    }, [push]
-  )
+    },
+    [push],
+  );
 
   return (
     <section>
@@ -50,11 +60,27 @@ const Category = () => {
         renderCell={renderCell}
         columns={COLUMN_LISTS_CATEGORY}
         data={[
-          {_id: "123", name: "Category 1", description: "Description 1", icon: "/images/general/logo.png"}
-        ]}>
-      </DataTable>
+          {
+            _id: "123",
+            name: "Category 1",
+            description: "Description 1",
+            icon: "/images/general/logo.png",
+          },
+        ]}
+        emptyContent="Category is empty"
+        limit={LIMIT_LISTS[0].label}
+        onChangeSearch={() => {}}
+        onClearSearch={() => {}}
+        onChangeLimit={() => {}}
+        onChangePage={() => {}}
+        currentPage={1}
+        buttonTopContentLabel="Create Category"
+        onClickButtonTopContent={() => {}}
+        totalPages={2}
+        // isLoading
+      />
     </section>
-  )
-}
+  );
+};
 
 export default Category;
